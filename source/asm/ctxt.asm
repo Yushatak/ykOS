c7323bf37cee69847556bce88c32c857ad42bf7b
Yushatak <yushatak@gmail.com>
2014-03-28 15:21:46 -0400

    Kernel Threading Fixed!
    Still bugs with returning to main thread, likely boot thread stored
    improperly or restored improperly, but the actual problems are solved.

--- source/asm/ctxt.asm
+++ source/asm/ctxt.asm
@@ -2,11 +2,10 @@ global ctxt_sw
 ;ctxt_sw(void **oldStack, void *newStack)
 [BITS 32]
 ctxt_sw:
-cli
+pushad
 lea eax, [esp+36]
-mov eax,esp
+mov [eax],esp
 mov eax,[esp+40]
 mov esp,eax
 popad
-sti
 ret
